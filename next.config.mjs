ba66245f4be9c228da137107304d2aee39bc0d3b
Buti Lamberto <butilamberto@Butis-MacBook-Air.local>
2025-12-29 02:27:28 +0100

    Allow Airtable image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "dl.airtable.com" },
+      { protocol: "https", hostname: "airtableusercontent.com" },
+      { protocol: "https", hostname: "v5.airtableusercontent.com" },
+      { protocol: "https", hostname: "v6.airtableusercontent.com" },
+    ],
+  },
+};
+
 export default nextConfig;
