ce311f035c53a9fd980e2b6d29e511d2e917e2c5
shorttermrentaleasy-cyber <shorttermrental.easy@gmail.com>
2026-07-24 19:12:57 +0200

    Chiudi correttamente la scheda socio

--- src/app/lvpeople/LVPeopleActions.tsx
+++ src/app/lvpeople/LVPeopleActions.tsx
@@ -3,14 +3,25 @@
 import React from "react";
 
 export default function LVPeopleActions() {
+  function closeMemberCard() {
+    window.close();
+
+    window.setTimeout(() => {
+      if (!window.closed) {
+        window.history.back();
+      }
+    }, 100);
+  }
+
   return (
     <div>
-      <a
-        href="/"
+      <button
+        type="button"
+        onClick={closeMemberCard}
         className="px-4 py-2 rounded-full border border-white/15 hover:border-white/30 hover:bg-white/10 text-xs tracking-[0.18em] uppercase"
       >
-        Home
-      </a>
+        Chiudi
+      </button>
     </div>
   );
 }
