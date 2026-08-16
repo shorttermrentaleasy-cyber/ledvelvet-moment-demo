c296a710fc3ae330d507b8bcc4bc6a3b9534380c
shorttermrentaleasy-cyber <shorttermrental.easy@gmail.com>
2026-08-16 21:06:47 +0200

    Attiva swipe mobile nella gallery Experience

--- src/app/moment2/page.tsx
+++ src/app/moment2/page.tsx
@@ -3,10 +3,12 @@ export const revalidate = false;
 
 import { Suspense } from "react";
 import Moment2Client from "./Moment2Client";
+import GallerySwipe from "./GallerySwipe";
 
 export default function Page() {
   return (
     <Suspense fallback={null}>
+      <GallerySwipe />
       <Moment2Client />
     </Suspense>
   );
