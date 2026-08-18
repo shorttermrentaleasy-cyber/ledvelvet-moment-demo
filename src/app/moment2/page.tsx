a8e4ce82a6a7308be5d141a6e1c26aa9e3910ffb
shorttermrentaleasy-cyber <shorttermrental.easy@gmail.com>
2026-08-18 18:19:41 +0200

    Mostra invito tesseramento agli account non soci

--- src/app/moment2/page.tsx
+++ src/app/moment2/page.tsx
@@ -4,11 +4,13 @@ export const revalidate = false;
 import { Suspense } from "react";
 import Moment2Client from "./Moment2Client";
 import GallerySwipe from "./GallerySwipe";
+import NonMemberCta from "./NonMemberCta";
 
 export default function Page() {
   return (
     <Suspense fallback={null}>
       <GallerySwipe />
+      <NonMemberCta />
       <Moment2Client />
     </Suspense>
   );
