e5f3181d67e8eb912130b89b980cc730ccfc4887
shorttermrentaleasy-cyber <shorttermrental.easy@gmail.com>
2026-08-18 18:39:30 +0200

    Mostra LEDVELVET in numbers in homepage

--- src/app/moment2/page.tsx
+++ src/app/moment2/page.tsx
@@ -5,12 +5,14 @@ import { Suspense } from "react";
 import Moment2Client from "./Moment2Client";
 import GallerySwipe from "./GallerySwipe";
 import MemberLoginGate from "./MemberLoginGate";
+import HomepageNumbers from "./HomepageNumbers";
 
 export default function Page() {
   return (
     <Suspense fallback={null}>
       <GallerySwipe />
       <MemberLoginGate />
+      <HomepageNumbers />
       <Moment2Client />
     </Suspense>
   );
